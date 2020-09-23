4d264b5accc65ca4e3e57c274d4fdc50efdf1621
emmalynnn97 <emmalynnn97@gmail.com>
2020-09-23 07:47:21 -0700

    tried to make the content more robust

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ function slideLeft() {
         sliderDotContainer.children[i].classList.remove('slider-dot-active')
 
         i = (i - 1) % slides.length;
+        //if the current slide has content, assign content to slide
+        if(slides[i].content){
+            sliderContent.innerHTML = slides[i].content;
+        }
+        //else clear out sliderContent
+        else{
+            sliderContent.innerHTML = '';
+        }
         //Make next dot active by adding the active class to it
         sliderDotContainer.children[i].classList.add('slider-dot-active')
 
@@ -15,9 +23,14 @@ function slideLeft() {
 function slideRight() {
     //Make current dot inactive by removing active class from it
     sliderDotContainer.children[i].classList.remove('slider-dot-active')
-
+    
     i = (i + 1) % slides.length;
-
+    if(slides[i].content){
+        sliderContent.innerHTML = slides[i].content;
+    }
+    else{
+        sliderContent.innerHTML = '';
+    }
     //Make next dot active by adding the active class to it
     sliderDotContainer.children[i].classList.add('slider-dot-active')
 
@@ -30,15 +43,18 @@ const sliderOverlay = document.querySelector('.slider-overlay');
 const sliderBtnRight = document.querySelector('.slider-btn-right');
 const sliderBtnLeft = document.querySelector('.slider-btn-left');
 const sliderDotContainer = document.querySelector('.slider-dot-container')
+const sliderContent = document.querySelector('.slider-content');
 const slides = [
     {
-        image: './slider-bg-2.jpg'
+        image: './slider-bg-2.jpg',
+        content: `<h1>Slide Header 1</h1><p>Slide copy text</p>`
     },
     {
         image: './slider-bg-3.jpg'
     },
     {
-        image: './slider-bg-4.jpg'
+        image: './slider-bg-4.jpg',
+        content: `<h1>Slide Header 3</h1><p>Lorem ipsum aksdhsajk hdjakshd asdg jka</p>`
     }
 ]
 //Dynamically generate a dot for each slide, and make the first dot the active one by default
@@ -56,7 +72,9 @@ for (var slideIndex = 0; slideIndex < slides.length; slideIndex++) {
 //Set default image as first one in array
 var i = 0;
 sliderContainer.style.backgroundImage = `url("${slides[0].image}")`
-
+if(slides[i].content){
+    sliderContent.innerHTML = slides[i].content;
+}
 //Have left button wait until click, on click cycle the slide backwards by 1 space
 sliderBtnLeft.addEventListener('click', slideLeft);
 
